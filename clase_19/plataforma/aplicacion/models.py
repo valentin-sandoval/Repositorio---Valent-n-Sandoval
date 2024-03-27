8c806c34eca0a527f2e5084a3e34b9064c8733a3
valentin sandoval <valentinsa2004qgmail.com>
2024-03-26 22:46:16 -0300

    borrar

--- clase_19/plataforma/aplicacion/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Reserva(models.Model):
-    nombre_de_usuario = models.CharField(max_length=50)
-    destino = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"Esta es una reserva a nombre de {self.nombre_de_usuario} con destino a {self.destino}"
--- clase_19/plataforma/aplicacion/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home_view, list_view, search_view, create_view
-
-urlpatterns = [
-    path("", home_view),
-    path("list/", list_view),
-    path("buscar/<nombre_de_usuario>", search_view),
-    path("crear/<nombre_de_usuario>/<destino>", create_view),
-]
